04be97942378b87aa94183eea3a03131e332abb5
Binary Wang <binarywang@gmail.com>
2020-09-08 09:36:39 +0800

    init

---
+++ src/main/java/com/binarywang/demo/wx/miniapp/WxMiniappDemoApplication.java
@@ -0,0 +1,15 @@
+package com.binarywang.demo.wx.miniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author binary wang
+ */
+@SpringBootApplication
+public class WxMiniappDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WxMiniappDemoApplication.class, args);
+    }
+
+}
